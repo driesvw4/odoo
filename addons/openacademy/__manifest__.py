2d747617c3c4fefc8657a091fdadb56fe8ca1fe7
driesvw4 <driesvanweyenbergh@hotmail.com>
2020-09-28 17:18:32 +0200

    making openacademy work finally
    basic views and course model are added. configuration of directories is good. (works as super user)

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -1,34 +1,37 @@
 # -*- coding: utf-8 -*-
 {
-    'name':        "OpenAcademy",
+    'name': "Open Academy",
 
-    'summary':
-                   """
-                   Openacademy
-                   """,
+    'summary': """Manage trainings""",
 
     'description': """
-        Manage course, classes, teachers, students, ...
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
     """,
 
-    'author':      "Odoo",
-    'website':     "http://www.odoo.com",
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/data/ir_module_category_data.xml
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category':    'OpenAcademy',
-    'version':     '0.1',
+    'category': 'Test',
+    'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends':     ['base'],
+    'depends': ['base'],
 
     # always loaded
-    'data':        [
-        "security/ir.model.access.csv",
-        "data/openacademy_data.xml",
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
-    'demo':        [],
-    'license': 'AGPL-3',
+    'demo': [
+        'demo.xml',
+    ],
 }
+
